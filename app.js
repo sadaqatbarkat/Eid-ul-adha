dadaddcff0cca60af4ae151856bd43117f392c87
sadaqatbarkat <153889463+sadaqatbarkat@users.noreply.github.com>
2024-05-06 13:39:31 +0500

    add this index css and js files

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 
 
 
-function abc (a,b,c,d,e){
-	document.getElementById("month").innerText = month
-	document.getElementById("days").innerText = days
-	document.getElementById("hours").innerText = hours
-	document.getElementById("minutes").innerText = minutes
-	document.getElementById("seconds").innerText = seconds
-
+function abc (){
 	
-}
-
 var eid =  new Date ("16 june 2024")
 var currentDate =  new Date()
 var find =  eid.getTime() -  currentDate.getTime()
@@ -20,14 +12,14 @@ var days = Math.floor( find/(1000*60*60*24))
 var hours = Math.floor(find/(1000*60*60))
 var minutes =  Math.floor(find/(1000*60))
 var seconds = Math.floor(find/(1000)) 
+	document.getElementById("month").innerText = month
+	document.getElementById("days").innerText = days
+	document.getElementById("hours").innerText = hours
+	document.getElementById("minutes").innerText = minutes
+	document.getElementById("seconds").innerText = seconds
 
+	
+}
 
-abc()
+setInterval(abc,1000)
 
-
-// console.log(Math.floor(find/(1000*60*60*24*30)))
-// console.log(Math.floor(find/(1000*60*60*24)))  
-// console.log(Math.floor(find/(1000*60*60)))
-// console.log(Math.floor(find/(1000*60)))
-
-// , find/(1000*60*60*24), find/(1000*60*60)  ,find/(1000*60)
